07ee1cc57ac1bae4d9c2eb099becf0f1cd404cf5
Numenot <qlv6719@gmail.com>
2022-02-01 19:02:42 +0100

    updated CORS policy

--- index.js
+++ index.js
@@ -24,7 +24,18 @@ app.use(morgan('common'));
 
 //CORS
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix-myflixapp.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //Authentication
 let auth = require('./auth')(app);
